e5ef2b825139c4d0e892259c4926582c4b030489
Ryan Guarasica <rguarascia@gmail.com>
2014-07-30 00:00:54 -0400

    Initial Commit
    The start.

---
+++ QR Encode/DataEncode.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QR_Encode
+{
+    class DataEncode
+    {
+        Dictionary<String, short> AlphaNum = new Dictionary<String, short>();
+
+        private void Alphanumeric()
+        {
+            AlphaNum.Add("0", 0);
+            AlphaNum.Add("1", 1);
+            AlphaNum.Add("2", 2);
+            AlphaNum.Add("3", 3);
+            AlphaNum.Add("4", 4);
+            AlphaNum.Add("5", 5);
+            AlphaNum.Add("6", 6);
+            AlphaNum.Add("7", 7);
+            AlphaNum.Add("8", 8);
+            AlphaNum.Add("9", 9);
+            AlphaNum.Add("A", 10);
+            AlphaNum.Add("B", 11);
+            AlphaNum.Add("C", 12);
+            AlphaNum.Add("D", 13);
+            AlphaNum.Add("E", 14);
+            AlphaNum.Add("F", 15);
+            AlphaNum.Add("G", 16);
+            AlphaNum.Add("H", 17);
+            AlphaNum.Add("I", 18);
+            AlphaNum.Add("J", 19);
+            AlphaNum.Add("K", 20);
+            AlphaNum.Add("L", 21);
+            AlphaNum.Add("M", 22);
+            AlphaNum.Add("N", 23);
+            AlphaNum.Add("O", 24);
+            AlphaNum.Add("P", 25);
+            AlphaNum.Add("Q", 26);
+            AlphaNum.Add("R", 27);
+            AlphaNum.Add("S", 28);
+            AlphaNum.Add("T", 29);
+            AlphaNum.Add("U", 30);
+            AlphaNum.Add("V", 31);
+            AlphaNum.Add("W", 32);
+            AlphaNum.Add("X", 33);
+            AlphaNum.Add("Y", 34);
+            AlphaNum.Add("Z", 35);
+            AlphaNum.Add(" ", 36);
+            AlphaNum.Add("$", 37);
+            AlphaNum.Add("%", 38);
+            AlphaNum.Add("*", 39);
+            AlphaNum.Add("+", 40);
+            AlphaNum.Add("-", 41);
+            AlphaNum.Add(".", 42);
+            AlphaNum.Add("/", 43);
+            AlphaNum.Add(":", 44);
+            //That was pain staking. 
+        }
+
+        private string EightBit(string Data)
+        {
+            byte[] theBytes =  Encoding.UTF8.GetBytes(Data);
+            StringBuilder myEncoded = new StringBuilder();
+            foreach (byte everyBit in theBytes)
+            {
+                myEncoded.Append(everyBit);
+            }
+            return myEncoded.ToString();
+        }
+    }
+}
---
+++ QR Encode/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace QR_Encode
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.rtbInput = new System.Windows.Forms.RichTextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(362, 376);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // rtbInput
+            // 
+            this.rtbInput.Location = new System.Drawing.Point(380, 12);
+            this.rtbInput.Name = "rtbInput";
+            this.rtbInput.Size = new System.Drawing.Size(212, 91);
+            this.rtbInput.TabIndex = 1;
+            this.rtbInput.Text = "";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(500, 365);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(92, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Encode";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 400);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.rtbInput);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmMain";
+            this.Text = "Encode";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox rtbInput;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
