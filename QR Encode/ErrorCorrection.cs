bb1bf08f8a8b9b692c24ee39f11b1645e064c0e1
Ryan Guarasica <rguarascia@gmail.com>
2014-08-02 00:51:49 -0400

    Added useless comments.

--- QR Encode/ErrorCorrection.cs
+++ QR Encode/ErrorCorrection.cs
@@ -11,7 +11,9 @@ namespace QR_Encode
         //This file will handle and process all Error Correction that is needed.
         private void CorrectionLevel(int messageLength)
         {
-
+            //TODO - Add version idenifier
+            //- Correction Level idenifier
+            //I love how I'm panning this out when I won't even use this comments...
         }
     }
 }
